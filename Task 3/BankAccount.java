6f555a6da8ee4f8e7dfff703fcf7f12c57303c55
Vivek Bhosale <135112044+bhosalevivek04@users.noreply.github.com>
2023-09-26 17:12:17 +0530

    BankAccount.java

---
+++ Task 3/BankAccount.java
@@ -0,0 +1,31 @@
+public class BankAccount {
+    private double balance;
+
+    public BankAccount(double initialBalance) {
+        this.balance = initialBalance;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+            System.out.println("Deposit of $" + amount + " successful.");
+        } else {
+            System.out.println("Invalid deposit amount.");
+        }
+    }
+
+    public boolean withdraw(double amount) {
+        if (amount > 0 && amount <= balance) {
+            balance -= amount;
+            System.out.println("Withdrawal of $" + amount + " successful.");
+            return true;
+        } else {
+            System.out.println("Invalid withdrawal amount or insufficient balance.");
+            return false;
+        }
+    }
+}
