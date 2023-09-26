3cfeb3e79318a7aa0dafe5c0b9f5f73c2168c3b8
Vivek Bhosale <135112044+bhosalevivek04@users.noreply.github.com>
2023-09-26 17:40:20 +0530

    Student.java

---
+++ Task 5/Student.java
@@ -0,0 +1,31 @@
+import java.io.Serializable;
+
+public class Student implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private int rollNumber;
+    private String name;
+    private String grade;
+
+    public Student(int rollNumber, String name, String grade) {
+        this.rollNumber = rollNumber;
+        this.name = name;
+        this.grade = grade;
+    }
+
+    public int getRollNumber() {
+        return rollNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getGrade() {
+        return grade;
+    }
+
+    @Override
+    public String toString() {
+        return "Roll Number: " + rollNumber + ", Name: " + name + ", Grade: " + grade;
+    }
+}
